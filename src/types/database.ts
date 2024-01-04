c70b066316adcc6634516e643148f69beb4e5d61
Alex Andru <alex007d@gmail.com>
2024-01-04 17:14:03 +0100

    Add Property type

--- src/types/database.ts
+++ src/types/database.ts
@@ -1,8 +1,24 @@
-import type { Collection } from 'mongodb';
+import type { Collection } from "mongodb";
 
 export interface User {
 	userId: number;
 	name: string;
+}
+
+export interface Property {
+	_id: string;
+	collectionName: "SaliSol Hills" | "SaliSol Resort" | "SaliSol Golf";
+	name: string;
+	price: number;
+	availability: boolean;
+	videoUrl: string;
+	thumbnailUrl: string;
+	albumUrls: string[];
+	builtMetersSquared: number;
+	plotMetersSquared: number;
+	dropboxUrl: string;
+	telegramContactUrl: string;
+	websiteUrl: string;
 }
 
 export interface Chat {
